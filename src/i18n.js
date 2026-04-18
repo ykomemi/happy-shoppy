807e5761d7ddfa09ae737fbd0c23a7e8bcf4a9d7
@Yuval.Komemi <yuval.komemi@eurac.edu>
2026-04-18 22:53:34 +0200

    🌍 i18n: EN/ES/CA static translations + multilingual voice via GPT-4o

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import en from "./locales/en.json";
+import es from "./locales/es.json";
+import ca from "./locales/ca.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+      ca: { translation: ca },
+    },
+    fallbackLng: "en",
+    detection: {
+      order: ["localStorage", "navigator"],
+      lookupLocalStorage: "shopLanguage",
+      caches: ["localStorage"],
+    },
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
